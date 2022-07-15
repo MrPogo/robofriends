84524d3b5281e02a7027894828a3747b4033baf8
Vaidas <vajjdaz@gmail.com>
2022-07-15 08:54:07 +0300

    update2

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-//import App from './App';
+import App from './App';
 import reportWebVitals from './reportWebVitals';
 import Card from './Card';
 import 'tachyons';
 import { robots } from './robots';
+import CardList from './CardList';
 
 /*const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -15,12 +16,7 @@ root.render(
 ); */
 
 ReactDOM.render(
-  <div>
-    <Card id = {robots[0].id} name= {robots[0].name} email={robots[0].email} />
-    <Card id = {robots[1].id} name= {robots[1].name} email={robots[1].email}  />
-    <Card id = {robots[2].id} name= {robots[2].name} email={robots[2].email}  />
-    <Card id = {robots[3].id} name= {robots[3].name} email={robots[3].email}  />
-  </div>
+  <App />
 
 , document.getElementById('root'));
 
