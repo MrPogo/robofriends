7ecbceaea482dd01e65d88716b60f7963bceedc0
Vaidas <vajjdaz@gmail.com>
2022-08-30 20:53:56 +0300

    Updates

---
+++ precache-manifest.0babf001129d4618955a5807756650a0.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "c14a5181352680a28df9",
+    "url": "/robofriends/static/css/main.0287cee9.chunk.css"
+  },
+  {
+    "revision": "c14a5181352680a28df9",
+    "url": "/robofriends/static/js/main.c14a5181.chunk.js"
+  },
+  {
+    "revision": "f99921d68d7ebc942685",
+    "url": "/robofriends/static/js/1.f99921d6.chunk.js"
+  },
+  {
+    "revision": "b4ac28a5b32053988a4d",
+    "url": "/robofriends/static/css/2.f40cf989.chunk.css"
+  },
+  {
+    "revision": "b4ac28a5b32053988a4d",
+    "url": "/robofriends/static/js/2.b4ac28a5.chunk.js"
+  },
+  {
+    "revision": "69d7087b8b0ba8fee442",
+    "url": "/robofriends/static/js/runtime~main.69d7087b.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "f63dd75494bc550a6fb6f14ed3defd41",
+    "url": "/robofriends/index.html"
+  }
+];
